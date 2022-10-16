f4697a1f0880de5c598fd230b0a6699c92f142ef
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-10-16 12:48:04 +0100

    feat: week 4 exercises

--- app/src/main/java/com/jordharr/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/com/jordharr/placemark/activities/PlacemarkActivity.kt
@@ -14,6 +14,7 @@ import timber.log.Timber.Forest.i
 class PlacemarkActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
+    var edit = false
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +26,28 @@ class PlacemarkActivity : AppCompatActivity() {
         app = application as MainApp
 
         if (intent.hasExtra("placemark_edit")) {
+            edit = true
             placemark = intent.extras?.getParcelable("placemark_edit")!!
             binding.placemarkTitle.setText(placemark.title)
             binding.description.setText(placemark.description)
+            binding.btnAdd.setText(R.string.save_placemark)
         }
 
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.description.text.toString()
-            if (placemark.title.isNotEmpty()) {
-                app.placemarks.create(placemark.copy())
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
-                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+            if (placemark.title.isEmpty()) {
+                Snackbar.make(it,R.string.enter_placemark_title, Snackbar.LENGTH_LONG)
                     .show()
+            } else {
+                if (edit) {
+                    app.placemarks.update(placemark.copy())
+                } else {
+                    app.placemarks.create(placemark.copy())
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
     }
 
