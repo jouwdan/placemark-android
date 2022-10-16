ef31bb9a01d2cf6aebda216e9e00a8cfc3787e0d
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-10-16 12:36:39 +0100

    feat: lab3 exercises

--- app/src/main/java/com/jordharr/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/com/jordharr/placemark/activities/PlacemarkActivity.kt
@@ -2,7 +2,10 @@ package com.jordharr.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import com.jordharr.placemark.R
 import com.jordharr.placemark.databinding.ActivityPlacemarkBinding
 import com.jordharr.placemark.main.MainApp
 import com.jordharr.placemark.models.PlacemarkModel
@@ -17,18 +20,15 @@ class PlacemarkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
         app = application as MainApp
-        i("Placemark Activity started...")
+
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.description.text.toString()
             if (placemark.title.isNotEmpty()) {
                 app.placemarks.add(placemark.copy())
-                i("add Button Pressed: ${placemark}")
-                for (i in app.placemarks.indices) {
-                    i("Placemark[$i]:${this.app.placemarks[i]}")
-                }
                 setResult(RESULT_OK)
                 finish()
             }
@@ -37,5 +37,17 @@ class PlacemarkActivity : AppCompatActivity() {
                     .show()
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> { finish() }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/jordharr/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/com/jordharr/placemark/activities/PlacemarkListActivity.kt
@@ -3,17 +3,13 @@ package com.jordharr.placemark.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.jordharr.placemark.R
+import com.jordharr.placemark.adapters.PlacemarkAdapter
 import com.jordharr.placemark.databinding.ActivityPlacemarkListBinding
-import com.jordharr.placemark.databinding.CardPlacemarkBinding
 import com.jordharr.placemark.main.MainApp
-import com.jordharr.placemark.models.PlacemarkModel
 
 class PlacemarkListActivity : AppCompatActivity() {
 
@@ -47,32 +43,5 @@ class PlacemarkListActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-}
-
-class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) :
-    RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardPlacemarkBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val placemark = placemarks[holder.adapterPosition]
-        holder.bind(placemark)
-    }
-
-    override fun getItemCount(): Int = placemarks.size
-
-    class MainHolder(private val binding : CardPlacemarkBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(placemark: PlacemarkModel) {
-            binding.placemarkTitle.text = placemark.title
-            binding.description.text = placemark.description
-        }
     }
 }
